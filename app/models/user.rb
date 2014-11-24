84ddce5982fc5b4ff1202dbb14e310f87256a575
Nataly Daniela Ortiz Arias <naty.dany.98@gmail.com>
2014-11-24 16:13:11 -0400

    password ontrola numeros mayus y min

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+	VALID_PASSWORD = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)./i
+
 	attr_accessible :password,:cuenta,:nombre,:password_confirmation
 	validates :cuenta, presence: true, uniqueness: {case_sensitive: false}
 	validates :nombre, presence: true, length: { minimum: 10}
-	validates :password, presence: true, length: { minimum: 8 }
+	validates :password, presence: true, format: {with: VALID_PASSWORD }, length: { minimum: 8, maximum: 40 }
 	validates :password_confirmation, presence: true, length: { minimum: 8 }
 	has_secure_password
-
 end
