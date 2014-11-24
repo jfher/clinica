70117030984f26a0d8ad73342fccf1cece204eb9
Nataly Daniela Ortiz Arias <naty.dany.98@gmail.com>
2014-11-24 14:57:56 -0400

    vista principal

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, length: { minimum: 8 }
 	validates :password_confirmation, presence: true, length: { minimum: 8 }
 	has_secure_password
+
 end
