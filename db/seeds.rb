06c7d08ded69cb628a2dde2bed6b885960b7163c
Luis Maldonado <luisoscar.mc@gmail.com>
2014-12-15 15:18:06 -0400

    roles_terminados

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.new({:cuenta => "secretaria", :nombre => "secretaria",:password => "Clinica5458.", :password_confirmation => "Clinica5458.", :rol => "secretaria", :control => "true"}).save(:validate => false)
-User.new({:cuenta => "oscar", :nombre => "luisoscarm",:password => "Clinica9398.", :password_confirmation => "Clinica9398.", :rol => "paciente", :control => "false"}).save(:validate => false)
+User.new({:cuenta => "bioquimica", :nombre => "bioquimica",:password => "Clinica1234.", :password_confirmation => "Clinica1234.", :rol => "bioquimica", :control => "true"}).save(:validate => false)
+User.new({:cuenta => "lomc", :nombre => "Luis Oscar Maldonado Castellon",:password => "Clinica9398.", :password_confirmation => "Clinica9398.", :rol => "paciente", :control => "false"}).save(:validate => false)
+User.new({:cuenta => "lfag", :nombre => "Luis Fernando Aruquipa Gamara",:password => "Clinica9893.", :password_confirmation => "Clinica9893.", :rol => "paciente", :control => "false"}).save(:validate => false)
+User.new({:cuenta => "noa", :nombre => "Nataly Ortiz Arias",:password => "Clinica4321.", :password_confirmation => "Clinica4321.", :rol => "paciente", :control => "false"}).save(:validate => false)
